86c0a83dd2e393d34e3f1a051c0077678a372744
dannyCHW <dannydt1697@gmail.com>
2021-10-02 17:51:22 +0800

    login init

--- adminLogin.php
+++ adminLogin.php
@@ -2,34 +2,34 @@
 
 require_once('connectDB.php');
 
-if(isset($_POST['teacherLogin'])){
+if(isset($_POST['adminLogin'])){
     $username = $_POST['username'];
     $pwd = $_POST['password'];
     
-    $sql = "SELECT teacherUsername, teacherPassword FROM teacher WHERE teacherUsername='$username'";
+    $sql = "SELECT adminUsername, adminPassword FROM admin WHERE adminUsername='$username'";
     $rs = mysqli_query($conn, $sql)or die(mysqli_error($conn));
     if(mysqli_num_rows($rs) <= 0){
 
         echo "<script type='text/javascript'>
         alert('Invaild! User does not exist.');
-        window.location.href = 'TeacherLogin.html';
+        window.location.href = 'adminLogin.html';
         </script>";
         
     } else {
         $rc = mysqli_fetch_assoc($rs);
 
-        if($rc['teacherPassword'] != $pwd){
+        if($rc['adminPassword'] != $pwd){
             
             echo "<script type='text/javascript'>
             alert('Wrong username or password');
-            window.location.href = 'TeacherLogin.php';
+            window.location.href = 'adminLogin.php';
             </script>";
             
 
         } else {
 
             echo "<script type='text/javascript'>
-            window.location.href = 'teacherLobby.html';
+            window.location.href = 'adminLobby.html';
             </script>";
 
         }
@@ -43,4 +43,6 @@ if(isset($_POST['teacherLogin'])){
     echo"alert('Something wrong');";
 
 }
+
+
 ?>
