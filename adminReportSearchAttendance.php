85d0816d9de8b2480d7c29c1322fc39324f5e1af
dannyCHW <dannydt1697@gmail.com>
2021-10-15 04:08:02 +0800

    adminReportSearch init

---
+++ adminReportSearchAttendance.php
@@ -0,0 +1,24 @@
+<?php 
+require_once('connectDB.php');
+
+$sql = "SELECT * FROM attanence ";
+    $rs = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+    
+    $arr = (array) null;
+
+    while ($row = mysqli_fetch_assoc($rs)){
+
+        $myObj = new stdClass();
+        $myObj->attanenceID = $row["attanenceID"];
+        $myObj->attanence_status = $row["attanence_status"];
+        $myObj->attanence_date	= $row["attanence_date"];
+        $myObj->updateTime	= $row["updateTime"];
+        $myObj->classID	= $row["classID"];
+        $myObj->studentID	= $row["studentID"];
+
+        array_push($arr, $myObj);  
+    }
+    
+    $myJson = json_encode($arr);
+        echo $myJson;
+?>
