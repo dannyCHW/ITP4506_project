a03ab086dc2fbd7b8b001c44c74860fe641be642
simonhkg2002@gmail.com <simonhkg2002@gmail.com>
2021-11-19 13:07:15 +0800

    fix

--- adminLobbyHTML.php
+++ adminLobbyHTML.php
@@ -187,7 +187,7 @@
       });
 
       $(document).on('click', '#btnDestory', function() {
-        var r = confirm("Press a button!");
+        var r = confirm("You really want to delete all board?");
         if (r == true) {
           $.ajax({
              type: 'post',
